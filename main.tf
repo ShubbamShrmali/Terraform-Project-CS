82507afd84880b3aa44990b678a4f1688ce2ff63
ShubbamShrmali <111654213+ShubbamShrmali@users.noreply.github.com>
2022-10-30 21:49:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ provider "aws" {
 resource "aws_instance" "tomcat" {
     ami = "ami-026b57f3c383c2eec"
     count = "1"
-    key_name   = "Jenkins.pem"
+    key_name   = "Jenkins"
     instance_type = "t2.micro"
-    security_groups = "sg-0edcb99a3be5a167c"
+    security_groups = ["sg-0edcb99a3be5a167c"]
     tags = {
         Name = "JenkinsServer"
     } 
